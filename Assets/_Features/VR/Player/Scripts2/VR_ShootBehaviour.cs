ce5d03c1eab3fcffc6df024e94811be55467fdf0
SalleVR-2 <epoc4oftime@gmail.com>
2020-11-30 11:47:05 +0100

    Improved guard death (they stay dead)

--- Assets/_Features/VR/Player/Scripts2/VR_ShootBehaviour.cs
+++ Assets/_Features/VR/Player/Scripts2/VR_ShootBehaviour.cs
@@ -41,7 +41,7 @@ namespace Gameplay.VR.Player
                 Debug.Log("I shot and hit " + hitInfo.collider.gameObject.name);
 
                 if (hitInfo.collider.CompareTag("Enemy"))
-                    hitInfo.collider.GetComponent<GuardBehaviour>().Shot();
+                    hitInfo.collider.GetComponent<AgentDeath>().Die();
             }
 
             else Debug.Log("I shot but didn't hit anything");
