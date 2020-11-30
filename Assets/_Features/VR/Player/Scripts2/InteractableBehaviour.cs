0edca5d432c50acd92448cc691a2d24e842f0804
Léonard <epoc4oftime@gmail.com>
2020-11-30 01:36:41 +0100

    New Sound Object_Volume Dependent on Impact Force

--- Assets/_Features/VR/Player/Scripts2/InteractableBehaviour.cs
+++ Assets/_Features/VR/Player/Scripts2/InteractableBehaviour.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 namespace Gameplay.VR
 {
     [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(AudioSource))]
+
     public class InteractableBehaviour : MonoBehaviour
     {
         // accessed by the Grab Behaviour
         internal Rigidbody rigidBody;
+        AudioSource audioSource;
+        private bool canPlaySound;
 
         private void Awake()
         {
             if (gameObject.tag != "Interactable") gameObject.tag = "Interactable";
             rigidBody = GetComponent<Rigidbody>();
+            audioSource = GetComponent<AudioSource>();
         }
 
         private void OnCollisionEnter(Collision collision)
         {
-            float collisionForce = collision.impulse.magnitude / Time.fixedDeltaTime;
-
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Environment"))
-                MakeNoise(collisionForce);
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Environment") && canPlaySound)
+                MakeNoise(collision.relativeVelocity.magnitude);
         }
 
         private void OnCollisionExit(Collision collision)
@@ -31,12 +34,16 @@ namespace Gameplay.VR
 
         private void ArmSound()
         {
-            //Debug.Log("Sound Object is armed");
+            canPlaySound = true;
         }
 
         private void MakeNoise(float collisionForce)
         {
             Debug.Log("I hit the target at a force of " + collisionForce);
+            audioSource.volume = collisionForce/20f;
+            audioSource.Play();
+
+            canPlaySound = false;
         }
     }
 }
