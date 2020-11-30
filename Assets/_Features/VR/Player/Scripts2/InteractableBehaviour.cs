12cb9612745a62ce5ea3c4d8745bbab261b15062
Léonard <epoc4oftime@gmail.com>
2020-11-30 01:14:50 +0100

    Working on new Sound Object

--- Assets/_Features/VR/Player/Scripts2/InteractableBehaviour.cs
+++ Assets/_Features/VR/Player/Scripts2/InteractableBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Gameplay.VR
 {
@@ -12,6 +13,30 @@ namespace Gameplay.VR
         {
             if (gameObject.tag != "Interactable") gameObject.tag = "Interactable";
             rigidBody = GetComponent<Rigidbody>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            float collisionForce = collision.impulse.magnitude / Time.fixedDeltaTime;
+
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Environment"))
+                MakeNoise(collisionForce);
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Environment"))
+                ArmSound();
+        }
+
+        private void ArmSound()
+        {
+            //Debug.Log("Sound Object is armed");
+        }
+
+        private void MakeNoise(float collisionForce)
+        {
+            Debug.Log("I hit the target at a force of " + collisionForce);
         }
     }
 }
