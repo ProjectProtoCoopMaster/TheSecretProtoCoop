2546ac89c3669f705a0214799cdf063dd768ce8d
Maxence <maxdurlon@gmail.com>
2020-12-01 14:05:04 +0100

    Test : NavMesh Dynamic Obstacles / Conclusion : Yes it works
    And with no extra problems. It's fairly simple.

---
+++ Assets/_Features/AI/Scripts/Objects/ObjectMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+    public float speed;
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+    private Vector3 currentDestination { get { if (onReturn) return startPoint.position; else return endPoint.position; } }
+
+    public bool onReturn { get; set; }
+
+    void Update()
+    {
+        MoveTo(currentDestination);
+    }
+
+    void MoveTo(Vector3 destination)
+    {
+        Vector3 localDestination = destination - transform.position;
+        Vector3 direction = Vector3.ClampMagnitude(localDestination, 1f);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (transform.position == destination) onReturn = !onReturn;
+    }
+}
