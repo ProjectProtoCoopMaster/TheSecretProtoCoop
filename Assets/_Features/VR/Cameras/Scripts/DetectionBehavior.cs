48976373c37b148e75e3d0c23488e7a79f54d65b
Léonard Gombert <epoc4oftime@gmail.com>
2020-12-11 00:42:05 +0100

    Added Emissive Light for VR Player Feedback on lights

--- Assets/_Features/VR/Cameras/Scripts/DetectionBehavior.cs
+++ Assets/_Features/VR/Cameras/Scripts/DetectionBehavior.cs
@@ -10,14 +10,11 @@ namespace Gameplay.VR
         [HideInInspector] public Transform playerHead;
         private bool detectedPlayer = false; 
         private RaycastHit hitInfo;
-        public Light powerLight;
 
         new void Awake()
         {
             base.Awake();
             playerHead = GameObject.Find("HeadCollider").transform;
-            powerLight = transform.parent.GetComponentInChildren<Light>();
-            //pingFrequency = 10;
         }
 
         private void Update()
@@ -91,18 +88,15 @@ namespace Gameplay.VR
         }
 
         #region Mobile Camera Power
-        [Button]
         // called from VR_CameraBehavior
         public void UE_DetectionOn()
         {
             poweredOn = true;
-            powerLight.enabled = true;
         }
-        [Button]
+
         public void UE_DetectionOff()
         {
             poweredOn = false;
-            powerLight.enabled = false;
         }
         #endregion
     }
