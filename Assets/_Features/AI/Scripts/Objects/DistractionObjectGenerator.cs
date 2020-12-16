563583d7c7659a64b8e8c9dc2316838f23896de7
Maxence <maxdurlon@gmail.com>
2020-12-16 16:44:17 +0100

    Distraction Object Generator

---
+++ Assets/_Features/AI/Scripts/Objects/DistractionObjectGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.VR
+{
+    public class DistractionObjectGenerator : MonoBehaviour
+    {
+        public List<InteractableBehaviour> distractionObjects;
+
+        public Transform dispenserPool;
+        public Transform dispenserSpawn;
+
+        public GameObject accessTrap;
+        private bool isOpen;
+
+        private float currentTime;
+
+        void Start()
+        {
+            SpawnObject();
+        }
+
+        public void PoolObject(GameObject gameObject)
+        {
+            gameObject.transform.parent = dispenserPool.transform;
+        }
+
+        public void SpawnObject()
+        {
+            accessTrap.SetActive(false);
+
+            currentTime = 1f;
+            isOpen = true;
+        }
+
+        void Update()
+        {
+            if (isOpen)
+            {
+                if (currentTime <= 0.0f)
+                {
+                    accessTrap.SetActive(true);
+
+                    GameObject distractionObject = dispenserPool.GetChild(0).gameObject;
+
+                    distractionObject.transform.parent = dispenserSpawn;
+                    distractionObject.transform.localPosition = Vector3.zero;
+
+                    isOpen = false;
+                }
+
+                currentTime -= Time.deltaTime;
+            }
+        }
+
+        IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(1f);
+        }
+    } 
+}
