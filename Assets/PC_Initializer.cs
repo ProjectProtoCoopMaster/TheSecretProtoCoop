c357377622d01cb0dfbcbf8391ec69615c20f32a
Léonard Gombert <epoc4oftime@gmail.com>
2020-12-22 00:19:11 +0100

    Auto-enable PC behaviours for player interactions

---
+++ Assets/PC_Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PC_Initializer : MonoBehaviour
+{
+    public List<MonoBehaviour> scripts;
+
+    private void Awake()
+    {
+        if(XRSettings.enabled == true)
+            foreach (MonoBehaviour item in scripts)
+                item.enabled = false;
+
+        if (XRSettings.enabled == false)
+            foreach (MonoBehaviour item in scripts) 
+                item.enabled = true;
+    }
+}
