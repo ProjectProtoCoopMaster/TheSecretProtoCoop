f250d9758dbd99f6508579f52bed6db48991cdeb
Quentin Milana <quentin.milana@hotmail.fr>
2021-01-04 19:01:50 +0100

    Vision Cones working

--- Assets/GP/Features/Cameras/Mobile/Scripts/VisionCone.cs
+++ Assets/GP/Features/Cameras/Mobile/Scripts/VisionCone.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace Gameplay.Mobile
 {
@@ -12,45 +13,48 @@ namespace Gameplay.Mobile
         public int numberVertices;
         public float angle;
         private float newAngle;
+        private  Vector3 newVerticePos;
+        Vector3[] vertices;
+        public  List<Vector3> newVerticesPos;
+        public int[] triangles;
 
         private void Start()
         {
-            //mesh = new Mesh();
-            //GetComponent<MeshFilter>().mesh = mesh;
-            //mesh.vertices = vertices;
-            //mesh.uv = uv;
-            //mesh.triangles = triangles;
+            mesh = new Mesh();
 
+            GetComponent<MeshFilter>().mesh = mesh;
         }
-
 
         private void Update()
         {
 
-            mesh = new Mesh();
-            GetComponent<MeshFilter>().mesh = mesh;
-
-            Vector3[] vertices = new Vector3[numberVertices];
-            int[] triangles = new int[(numberVertices * 3)];
+            vertices = new Vector3[numberVertices];
+            triangles = new int[(numberVertices * 3)];
+            newAngle = angle / (float)(numberVertices);
+            newVerticesPos.Clear();
             vertices[0] = transform.position;
-            newAngle = angle / (float)(numberVertices - 1);
-
+            newVerticesPos.Add(vertices[0]);
             for (int i = 1; i < numberVertices; ++i)
             {
                 RaycastHit hit;
-                //Vector3 axis = Quaternion.AngleAxis(((newAngle + transform.eulerAngles.y) * (float)(i - 1)), transform.up) * transform.forward * radius;
-                Vector3 newVerticePos = Quaternion.AngleAxis((newAngle) * (float)(i - 1), transform.up) * transform.forward * radius;
-                if (Physics.Raycast(transform.position, newVerticePos, out hit, radius))
+                newVerticePos = Quaternion.AngleAxis((newAngle) * (float)(i - 1), transform.up) * transform.forward * radius ;
+                
+
+                if (Physics.Raycast(vertices[0], newVerticePos, out hit, radius))
                 {
-                    Debug.Log("hello");
                     vertices[i] = new Vector3(hit.point.x, vertices[i].y, hit.point.z);
+                   
                 }
                 else
                 {
-                    vertices[i] = newVerticePos;
-                }
+                    vertices[i] = newVerticePos + transform.position;
 
+                }
+                vertices[i] = transform.rotation * (vertices[i] - vertices[0]) + vertices[0];
+                newVerticesPos.Add(vertices[i]);
             }
+            
+
 
             for (int i = 0; i + 2 < numberVertices; ++i)
             {
@@ -60,24 +64,32 @@ namespace Gameplay.Mobile
                 triangles[index + 2] = i + 2;
             }
 
-            int lastTriangleIndex = triangles.Length - 3;
-            triangles[lastTriangleIndex + 0] = 0;
-            triangles[lastTriangleIndex + 1] = numberVertices - 1;
-            triangles[lastTriangleIndex + 2] = 1;
-
+            mesh.Clear();
             mesh.vertices = vertices;
             mesh.triangles = triangles;
+
         }
 
         private void OnDrawGizmos()
         {
+            if(newVerticesPos.Count != 0)
+            {
+                for (int i = 0; i < newVerticesPos.Count; i++)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawSphere(newVerticesPos[i], .1f);
+
+                }
+            }
+
             Vector3 axis = Quaternion.AngleAxis((transform.eulerAngles.y), transform.up) * transform.forward * radius;
-            Vector3 newVerticePos = Quaternion.AngleAxis((newAngle), transform.up) * transform.forward * radius;
             Gizmos.color = Color.red;
             Gizmos.DrawRay(transform.position, newVerticePos);
             Gizmos.color = Color.blue;
             Gizmos.DrawRay(transform.position, axis);
+
         }
+
     }
 }
 
