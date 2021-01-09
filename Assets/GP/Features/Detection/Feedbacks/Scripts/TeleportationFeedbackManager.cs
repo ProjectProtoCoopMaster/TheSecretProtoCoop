fbf641d390b7e2e4972f7c20959f294e4b19e14d
Léonard Gombert <epoc4oftime@gmail.com>
2021-01-09 17:28:06 +0100

    URP Pipeline_Customized for Neutral colors

--- Assets/GP/Features/Detection/Feedbacks/Scripts/TeleportationFeedbackManager.cs
+++ Assets/GP/Features/Detection/Feedbacks/Scripts/TeleportationFeedbackManager.cs
@@ -1,4 +1,5 @@
-﻿using Gameplay.VR.Player;
+﻿#if UNITY_STANDALONE
+using Gameplay.VR.Player;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -65,3 +66,4 @@ namespace Gameplay.VR.Feedbacks
         }
     }
 }
+#endif
