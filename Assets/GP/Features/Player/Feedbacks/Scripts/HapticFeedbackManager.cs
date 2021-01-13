88f01f8d0d7c5e1df453c92207d309e642716a5a
Léonard <epoc4oftime@gmail.com>
2021-01-13 12:17:46 +0100

    Created a VR FEEDBACKS CONTAINER obj + HAPTIC FEEDBACKS for VR Player

---
+++ Assets/GP/Features/Player/Feedbacks/Scripts/HapticFeedbackManager.cs
@@ -0,0 +1,39 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HapticFeedbackManager : MonoBehaviour
+{
+    [SerializeField] private SteamVR_Action_Vibration hapticAction;
+
+    [SerializeField] [FoldoutGroup("Player Weapon Variables")] private float gunshotDuration, gunshotFrequency, gunshotAmplitude;
+    [SerializeField] [FoldoutGroup("Player Weapon Variables")] private SteamVR_Input_Sources weaponHand;
+
+    [SerializeField] [FoldoutGroup("UI Click Variables")] private float uIClickDuration, uIClickFrequency, uIClickAmplitude;
+    [SerializeField] [FoldoutGroup("UI Click Variables")] private SteamVR_Input_Sources uIClickHand;
+
+    [SerializeField] [FoldoutGroup("UI Hover Variables")] private float uIHoverDuration, uIHoverFrequency, uIHoverAmplitude;
+    [SerializeField] [FoldoutGroup("UI Hover Variables")] private SteamVR_Input_Sources uIHoverHand;
+
+    public void GE_Gunshot()
+    {
+        Pulse(gunshotDuration, gunshotFrequency, gunshotAmplitude, weaponHand);
+    }
+
+    public void GE_UIClick()
+    {
+        Pulse(uIClickDuration, uIClickFrequency, uIClickAmplitude, uIClickHand);
+    }
+
+    public void GE_UIHover()
+    {
+        Pulse(uIHoverDuration, uIHoverFrequency, uIHoverAmplitude, uIHoverHand);
+    }
+
+    private void Pulse(float duration, float frequency, float amplitude, SteamVR_Input_Sources inputSource)
+    {
+        hapticAction.Execute(0, duration, frequency, amplitude, inputSource);
+    }
+}
