14a9ad46c06b191936f812788fbab053a65f59d9
Léonard <epoc4oftime@gmail.com>
2021-01-18 16:08:48 +0100

    Fixed AreaGlowing

--- Assets/GP/Features/Player/VR/Scripts/TeleportationArea.cs
+++ Assets/GP/Features/Player/VR/Scripts/TeleportationArea.cs
@@ -1,6 +1,7 @@
 ﻿#if UNITY_STANDALONE
+using Sirenix.OdinInspector;
+using System.Collections;
 using UnityEngine;
-using Valve.VR;
 
 namespace Gameplay.VR.Player
 {
@@ -9,7 +10,50 @@ namespace Gameplay.VR.Player
     [RequireComponent(typeof(MeshCollider))]
     public class TeleportationArea : MonoBehaviour
     {
+        Material teleportationMaterial;
+        [SerializeField] float lerpDuration = .75f;
+        float timeElapsed;
+        float startValue = 0, endValue = 1;
+        float valueToLerp;
 
+        void Awake()
+        {
+            teleportationMaterial = GetComponent<MeshRenderer>().material;
+        }
+
+        public void On()
+        {
+            startValue = 0;
+            endValue = 1;
+
+            timeElapsed = 0f;
+
+            StartCoroutine(Lerp());
+        }
+
+        public void Off()
+        {
+            startValue = 1;
+            endValue = 0;
+
+            timeElapsed = 0f;
+
+            StartCoroutine(Lerp());
+        }
+
+        IEnumerator Lerp()
+        {
+            while (timeElapsed < lerpDuration)
+            {
+                valueToLerp = Mathf.Lerp(startValue, endValue, timeElapsed / lerpDuration);
+                timeElapsed += Time.deltaTime;
+                teleportationMaterial.SetFloat("_GlowState", valueToLerp);
+                yield return null;
+            }
+
+            valueToLerp = endValue;
+            teleportationMaterial.SetFloat("_GlowState", valueToLerp);
+        }
     } 
 }
 #endif
