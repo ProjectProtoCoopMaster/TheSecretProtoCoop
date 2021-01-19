7cb32a0d71746b65eb00d8ba66fe1495ac62b50b
SalleVR-2 <epoc4oftime@gmail.com>
2021-01-19 13:46:38 +0100

    v0.421

--- Assets/GP/Features/Player/Mobile/Scripts/PlayerBehavior.cs
+++ Assets/GP/Features/Player/Mobile/Scripts/PlayerBehavior.cs
@@ -25,8 +25,9 @@ namespace Gameplay.Mobile
             {
                 _transform.position = _playerPosition.Value; /*+ centerTransform.position;*/
                 //_transform.forward = _player.Value.transform.forward;
-               // _transform.eulerAngles = new Vector3(0, _transform.eulerAngles.y, 0);
+                
                 transform.rotation = _playerRotation.Value;
+                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
             }
         }
     }
