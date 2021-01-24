8270c4047f9c37fdcfd5bbdcc7b0c50f9fafcf4e
Quentin Milana <quentin.milana@hotmail.fr>
2021-01-24 21:21:06 +0100

    Jammer Glow + fixes

--- Assets/GP/Features/Player/VR/Scripts/PlayerBehavior.cs
+++ Assets/GP/Features/Player/VR/Scripts/PlayerBehavior.cs
@@ -48,7 +48,7 @@ namespace Gameplay.VR
         IEnumerator Start()
         {
             _OnStart.Invoke();
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(3.0f);
             _refreshScene.Raise();
         }
 
