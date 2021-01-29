5bff68096045af0dc1a18b149aeb4f67b3394dec
Léonard Gombert <epoc4oftime@gmail.com>
2021-01-29 12:23:36 +0100

    Auto-assign TeleportArea materials w/ ctrl + T

---
+++ Assets/GP/Tools/Level Design Tools/TeleportAreaAssigner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Gameplay.VR.Player;
+
+public class TeleportAreaAssigner : Editor
+{
+    TeleportationArea[] teleportationAreas;
+
+    [MenuItem("Tools/Assign Teleport Materials %t")]
+    private void AssignTeleportationAreas()
+    {
+        teleportationAreas = FindObjectsOfType<TeleportationArea>();
+
+        for (int i = 0; i < teleportationAreas.Length; i++)
+        {
+            teleportationAreas[i].GetComponent<Renderer>().material = (Material)AssetDatabase.LoadAssetAtPath("Assets/GP/Features/Player/Feedbacks/Materials/PBR_TeleportArea.mat", typeof(Material));
+        }        
+    }
+}
