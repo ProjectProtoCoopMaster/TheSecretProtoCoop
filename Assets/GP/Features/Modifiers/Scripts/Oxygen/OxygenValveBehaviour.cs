079adbdf4402417f224bccd03d8147b61ae71375
Léonard <epoc4oftime@gmail.com>
2021-02-02 16:27:20 +0100

    A shit script to rotate a cube in VR

---
+++ Assets/GP/Features/Modifiers/Scripts/Oxygen/OxygenValveBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Gameplay.VR
+{
+    public class OxygenValveBehaviour : MonoBehaviour
+    {
+        [ReadOnly] [ShowInInspector] int currentValue, lastValue;
+
+        [ReadOnly] [ShowInInspector] int tickRate = 120;
+        [ReadOnly] [ShowInInspector] int framesPassed = 0;
+
+        private void FixedUpdate()
+        {
+            framesPassed++;
+
+            if (framesPassed % tickRate == 0)
+            {
+                lastValue = currentValue;
+                currentValue = (int)transform.rotation.eulerAngles.x;
+
+                if (currentValue < lastValue) Debug.Log("Full turn");
+
+                framesPassed = 0;
+            }
+        }
+    }
+}
