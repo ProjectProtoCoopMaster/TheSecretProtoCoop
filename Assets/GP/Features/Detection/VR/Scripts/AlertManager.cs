fd0afe0e7a419c7d4ed6d3248d843b69011e556a
Maxence <maxdurlon@gmail.com>
2021-02-10 14:46:01 +0100

    Alert

--- Assets/GP/Features/Detection/VR/Scripts/AlertManager.cs
+++ Assets/GP/Features/Detection/VR/Scripts/AlertManager.cs
@@ -13,27 +13,26 @@ namespace Gameplay.VR
 #if UNITY_STANDALONE
         public bool alert { get; set; }
 
-        [SerializeField] [FoldoutGroup("Player Detection State")] GameEvent playerSpotted, playerIncognito;
+        [SerializeField] GameEvent playerSpotted, playerIncognito;
+        [SerializeField] GameEvent gameOverAlarm;
 
-        [SerializeField] [FoldoutGroup("Alarm Raising")] float alarmRaiseDuration;
+        [SerializeField] [FoldoutGroup("Alarm Raising")] float alertDuration;
 
         [ReadOnly] [FoldoutGroup("Alarm Raising")]
         internal List<GuardManager> deadGuards = new List<GuardManager>();
         [ReadOnly] [FoldoutGroup("Alarm Raising")]
         internal List<GuardManager> alarmRaisers = new List<GuardManager>();
 
-        [SerializeField] [FoldoutGroup("Alarm Raising")] GameEvent gameOverAlarm;
-
-        [SerializeField] [FoldoutGroup("Debugging")] float timePassed;
+        private float currentTime;
 
         public LoseType loseType { get; set; }
 
-        #region Game Events
         public void Alert()
         {
             if (!alert)
             {
                 // Call the Alert Feedback Text
+                playerSpotted.Raise();
             }
             alert = true;
         }
@@ -47,73 +46,40 @@ namespace Gameplay.VR
         {
             alert = false;
             // Call Incognito Feedback Text
-            timePassed = 0.0f;
+            playerIncognito.Raise();
+            currentTime = 0.0f;
         }
-
-        public void GE_PlayerDetectedByGuard()
-        {
-            if (raisingAlarm != true)
-            {
-                raisingAlarm = true; // prevent the event from being raised more than once
-                playerSpotted.Raise();
-            }
-        }
-
-        public void GE_PlayerDetectedByCamera()
-        {
-            Debug.Log("Restarting the game, a camera saw me");
-            GameOver(LoseType.PlayerSpottedByCam);
-        }
-
-        public void GE_BodyDetectedByGuard()
-        {
-            if (raisingAlarm != true)
-            {
-                raisingAlarm = true; // prevent the event from being raised more than once
-                playerSpotted.Raise();
-            }
-        }
-
-        public void GE_BodyDetectedByCamera()
-        {
-            Debug.Log("Restarting the game, a camera saw a body");
-            GameOver(LoseType.BodySpottedByCam);
-        }
-        #endregion
 
         private void Update()
         {
             if (alert)
             {
                 // increase the time that is currently passing
-                timePassed += Time.unscaledDeltaTime;
+                currentTime += Time.unscaledDeltaTime;
 
                 // if you kill all the alarm raising entities
                 if (alarmRaisers.Count == 0) Incognito();
 
                 // if the alarm has passed its limit
-                if (timePassed >= alarmRaiseDuration)
+                if (currentTime >= alertDuration)
                 {
                     // if there are still entities raising the alarm, it's game over
                     if (alarmRaisers.Count > 0)
                     {
                         Detected();
-
-                        GE_RefreshScene();
                     }
                 }
             }
         }
 
-        public void GE_RefreshScene()
+        public void _Reset()
         {
             alert = false;
 
             deadGuards.Clear();
             alarmRaisers.Clear();
 
-            timePassed = 0f;
-            Time.timeScale = 1f;
+            currentTime = 0.0f;
         }
 #endif
     }
