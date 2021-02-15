0008df6b103dc5114ed5cf4b24bf9f659c676c71
Léonard <epoc4oftime@gmail.com>
2021-02-15 15:18:18 +0100

    Debug Mode turns the player into a Ghost.

--- Assets/GP/Features/Player/VR/Scripts/BonePositionTracker.cs
+++ Assets/GP/Features/Player/VR/Scripts/BonePositionTracker.cs
@@ -8,16 +8,22 @@ namespace Gameplay.VR.Player
     public class BonePositionTracker : MonoBehaviour
     {
         public Vector3Variable boneToTrack;
+        public BoolVariable _debuggingGame;
 
         int framesPassed;
         int frequency = 4;
+
+        private void Awake()
+        {
+            if (_debuggingGame) GetComponentInChildren<Collider>().enabled = false;
+        }
 
         private void Update()
         {
             framesPassed++;
             if (framesPassed % frequency == 0)
             {
-                boneToTrack.Value = transform.position;
+                if (_debuggingGame.Value != false) boneToTrack.Value = transform.position;
                 framesPassed = 0;
             }
         }
